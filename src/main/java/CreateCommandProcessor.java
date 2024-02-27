a5f0deea1576521cfe8aee79acb5137940263936
@reaidmottakinalief <reaidmottakinalief@gmail.com>
2024-02-27 13:02:36 -0500

    Improved Create and Deposit Processor

---
+++ src/main/java/CreateCommandProcessor.java
@@ -0,0 +1,15 @@
+public class CreateCommandProcessor {
+	private final Bank bank;
+
+	CreateCommandProcessor(Bank bank) {
+		this.bank = bank;
+	}
+
+	void execute(String[] commandArguments) {
+		if (commandArguments[1].equalsIgnoreCase("checking")) {
+			double apr = Double.parseDouble(commandArguments[3]);
+			bank.openCheckingAccount(commandArguments[2], apr);
+
+		}
+	}
+}
