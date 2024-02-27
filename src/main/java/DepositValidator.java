3e9cdacfcbde68eabb1e8909bad3c2baf645be02
@reaidmottakinalief <reaidmottakinalief@gmail.com>
2024-02-27 06:05:40 -0500

    fixed  java.lang.NoClassDefFoundError at Class.java:-2

--- src/main/java/DepositValidator.java
+++ src/main/java/DepositValidator.java
@@ -1,3 +1,74 @@
-public interface DepositValidator {
-	boolean validate(String command);
+class DepositValidator {
+
+	private final Bank bankingSystem;
+
+	DepositValidator(Bank bankingSystem) {
+		this.bankingSystem = bankingSystem;
+	}
+
+	static boolean checkAccountExists(String[] args, Bank bankingSystem) {
+		try {
+			return bankingSystem.getAccounts().containsKey(args[1]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	static boolean isCheckingAccountReference(String[] args, Bank bankingSystem) {
+		try {
+			Account acc = bankingSystem.getAccounts().get(args[1]);
+			return acc instanceof CheckingAccount;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	static boolean isSavingsAccountReference(String[] args, Bank bankingSystem) {
+		try {
+			Account acc = bankingSystem.getAccounts().get(args[1]);
+			return acc instanceof SavingsAccount;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	static boolean isCDAccountReference(String[] args, Bank bankingSystem) {
+		try {
+			Account acc = bankingSystem.getAccounts().get(args[1]);
+			return acc instanceof CDAccount;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	static double parseToDouble(String value) {
+		try {
+			return Double.parseDouble(value);
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+	}
+
+	boolean validate(String[] args) {
+		if (checkAccountExists(args, bankingSystem)) {
+			if (isCheckingAccountReference(args, bankingSystem)) {
+				return checkCheckingAccountDepositValidity(args);
+			} else if (isSavingsAccountReference(args, bankingSystem)) {
+				return checkSavingsAccountDepositValidity(args);
+			} else if (isCDAccountReference(args, bankingSystem)) {
+				return false; // CD Account deposits have specific restrictions.
+			}
+		}
+		return false;
+	}
+
+	private boolean checkCheckingAccountDepositValidity(String[] args) {
+		double depositAmount = parseToDouble(args[2]);
+		return (depositAmount >= 0 && depositAmount <= 1000);
+	}
+
+	private boolean checkSavingsAccountDepositValidity(String[] args) {
+		double depositAmount = parseToDouble(args[2]);
+		return (depositAmount >= 0 && depositAmount <= 2500);
+	}
 }
