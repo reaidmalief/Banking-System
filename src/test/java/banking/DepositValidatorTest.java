032a8680c0f62fe50defb8c15fef50164b12d16e
@reaidmottakinalief <reaidmottakinalief@gmail.com>
2024-03-07 14:14:37 -0500

    Test for invalid account id less than 8 char

--- src/test/java/banking/DepositValidatorTest.java
+++ src/test/java/banking/DepositValidatorTest.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.Test;
 public class DepositValidatorTest {
 	private CommandValidator commandValidator;
 	private InputParser inputParser;
+	private Bank bank;
 
 	@BeforeEach
 	void setUp() {
-		Bank bank = new Bank();
+		bank = new Bank();
 		commandValidator = new CommandValidator(bank);
 		inputParser = new InputParser();
 	}
@@ -38,6 +39,14 @@ public class DepositValidatorTest {
 	void account_ID_does_not_exists() {
 		String[] commandArgs = inputParser.parseCommand("deposit 98765432 100");
 		assertFalse(commandValidator.validate(commandArgs));
+	}
+
+	@Test
+	void invalid_account_ID_less_than_eight_digit() {
+		bank.openCheckingAccount("12345678", 0.1);
+		String[] commandArgs = inputParser.parseCommand("deposit 123456789 100");
+		assertFalse(commandValidator.validate(commandArgs));
+
 	}
 
 }
