66d21f657417db301222eb5fdd385ebd60d26d3b
@reaidmottakinalief <reaidmottakinalief@gmail.com>
2024-03-18 07:05:07 -0400

    fixed Method `validate` has a Cognitive Complexity of 11

--- src/main/java/banking/CommandValidator.java
+++ src/main/java/banking/CommandValidator.java
@@ -8,24 +8,47 @@ public class CommandValidator {
 	}
 
 	boolean validate(String[] commandArguments) {
-		if (commandArguments[0].equalsIgnoreCase("create")) {
-			AccountCreationCommandValidator validator = new AccountCreationCommandValidator(bank);
-			return validator.validate(commandArguments);
-		} else if (commandArguments[0].equalsIgnoreCase("deposit")) {
-			DepositCommandValidator validator = new DepositCommandValidator(bank);
-			return validator.validate(commandArguments);
-		} else if (commandArguments[0].equalsIgnoreCase("withdraw")) {
-			WithdrawCommandValidator validator = new WithdrawCommandValidator(bank);
-			return validator.validate(commandArguments);
-		} else if (commandArguments[0].equalsIgnoreCase("pass")) {
-			PassTimeCommandValidator validator = new PassTimeCommandValidator();
-			return validator.validate(commandArguments);
-		} else if (commandArguments[0].equalsIgnoreCase("transfer")) {
-			TransferCommandValidator validator = new TransferCommandValidator(bank);
-			return validator.validate(commandArguments);
-		} else {
+		if (commandArguments.length == 0) {
 			return false;
 		}
+		switch (commandArguments[0].toLowerCase()) {
+		case "create":
+			return validateCreate(commandArguments);
+		case "deposit":
+			return validateDeposit(commandArguments);
+		case "withdraw":
+			return validateWithdraw(commandArguments);
+		case "pass":
+			return validatePass(commandArguments);
+		case "transfer":
+			return validateTransfer(commandArguments);
+		default:
+			return false;
+		}
+	}
 
+	private boolean validateCreate(String[] commandArguments) {
+		AccountCreationCommandValidator validator = new AccountCreationCommandValidator(bank);
+		return validator.validate(commandArguments);
+	}
+
+	private boolean validateDeposit(String[] commandArguments) {
+		DepositCommandValidator validator = new DepositCommandValidator(bank);
+		return validator.validate(commandArguments);
+	}
+
+	private boolean validateWithdraw(String[] commandArguments) {
+		WithdrawCommandValidator validator = new WithdrawCommandValidator(bank);
+		return validator.validate(commandArguments);
+	}
+
+	private boolean validatePass(String[] commandArguments) {
+		PassTimeCommandValidator validator = new PassTimeCommandValidator();
+		return validator.validate(commandArguments);
+	}
+
+	private boolean validateTransfer(String[] commandArguments) {
+		TransferCommandValidator validator = new TransferCommandValidator(bank);
+		return validator.validate(commandArguments);
 	}
 }
